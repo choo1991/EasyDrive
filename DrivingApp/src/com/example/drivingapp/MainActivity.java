2cd2acd022659acbc83cdbb00d43bc50f905601c
Kameron Shey <Kameron.Shey@yahoo.com>
2014-02-27 01:02:31 -0800

    Add basics for location tracking

--- DrivingApp/src/com/example/drivingapp/MainActivity.java
+++ DrivingApp/src/com/example/drivingapp/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello World");
+        initLocationManager();
     }
 
 
@@ -40,4 +40,80 @@ public class MainActivity extends Activity {
     	startService(readAccel);
 
     }
+    
+    private void initLocationManager() {
+		// Acquire a reference to the system Location Manager
+		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+		// Define a listener that responds to location updates
+		LocationListener locationListener = new LocationListener() {
+			public void onLocationChanged(Location location) {
+				// Called when a new location is found by the network location provider.
+				makeUseOfNewLocation(location);
+			}
+
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+
+			}
+
+			public void onProviderEnabled(String provider) {
+
+			}
+
+			public void onProviderDisabled(String provider) {
+				// alert something saying we can't do anything if they don't give us permissions?
+			}
+		};
+		String locationProvider = LocationManager.GPS_PROVIDER;
+
+		// Register the listener with the Location Manager to receive location updates
+		locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
+
+		// gets last known location
+		//saving in case we want it later
+		//Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+	}
+
+	private void makeUseOfNewLocation(Location location) {
+		// TODO Auto-generated method stub
+		System.out.println("Latitude = " + location.getLatitude());
+		System.out.println("Longitude = " + location.getLongitude());
+		System.out.println("Speed = " + location.getSpeed());
+		// might be able to use time for determining speed
+		System.out.println("Time = " + location.getTime());
+		float speed = location.getSpeed();
+		System.out.println("Speed = " + speed);
+		checkIfDriving(location);
+	}
+	
+	private void checkIfDriving(Location location)
+	{
+		float speed = location.getSpeed();
+		long time = location.getTime();
+		// speed is returned in meters per second
+		// 10 mph ~ 4.4 meters per second
+		// this is 
+		if (speed > 4.5) {
+			initCustomLockScreen();
+		}
+	}
+	
+	// 
+	private void initCustomLockScreen(long time) {
+		// initialize custom lock screen here:
+		
+		// disable location manager since we don't want to continue
+		// to check location after home screen is locked
+		disableLocationManager();
+	}
+	
+	// we need a method that will delay the lock screen for X minutes until it checks again
+	// for the phone moving more than 10 miles an hour (or whatever speed we choose)
+	// how can we say... in X hours, restart location manager?
+	private void disableLocationManager() {
+		// do we want to keep track of time here so the app knows how long to disable location checking
+		// and knows when to start checking for speed again
+		locationManager.removeUpdates(locationListener);
+
+	}
 }
